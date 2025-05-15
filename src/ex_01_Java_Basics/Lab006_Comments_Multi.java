395f5f9888c665922b54921e22e0a46c0d39ed6d
Mahisau777 <maheshtarlekar@gmail.com>
2025-05-16 00:55:35 +0530

    first commit

---
+++ src/ex_01_Java_Basics/Lab006_Comments_Multi.java
@@ -0,0 +1,27 @@
+package ex_01_Java_Basics;
+
+public class Lab006_Comments_Multi {
+
+    /**
+     * Author : Mahesh Tarlekar
+     * Course :  ATB12x
+     * Learning :  Java
+     *
+     **/
+    public static void main(String[] args) {
+        /**
+         * Author : Mahesh Tarlekar
+         * Course :  ATB12x
+         * Learning :  Java
+         *
+         **/
+        System.out.println("Multi Comment");
+    }
+}
+/**
+ * Author : Pramod Dutta
+ * Course :  ATB12x
+ * Learning :  Java
+ *
+ **/
+
---
+++ src/ex_02_Java_Basics_Part2/Lab027_Multiple_Variables.java
@@ -0,0 +1,11 @@
+package ex_02_Java_Basics_Part2;
+
+public class Lab027_Multiple_Variables {
+
+    public static void main(String[] args) {
+
+        int a = 10, b = 23, c = 90;
+        int a1 = 10;
+        //System.out.println(_);
+    }
+}
